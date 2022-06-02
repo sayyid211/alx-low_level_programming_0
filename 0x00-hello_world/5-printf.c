a242006fb0ed5885cad2ef385a62ad4773499693
Adamu Muhammad <sayyid211@gmail.com>
2022-06-02 15:35:46 -0700

    update 0-preprocessor

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * Program to print some text
+ *
+ * The entry point is the int main section
+ * Prints about grammar and art
+ * return 0 returns 0 from main
+*/
+/**
+ * main function is the program's entry point
+ */
+int main(void)/* Entry point is main, return type is int*/
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	/**
+	 *return type for main
+	 */
+	return (0);
+}
