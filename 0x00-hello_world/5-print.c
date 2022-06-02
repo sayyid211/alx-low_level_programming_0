7e7d4f3a9c9b1bc21b29062c121903954ca711a9
Adamu Muhammad <sayyid211@gmail.com>
2022-06-02 14:49:15 -0700

    added printf.c

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,15 @@
+/**
+ * Program to print some text
+ */
+
+/* header file*/
+
+#include <stdio.h>
+
+/* the main function, the entry point*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
