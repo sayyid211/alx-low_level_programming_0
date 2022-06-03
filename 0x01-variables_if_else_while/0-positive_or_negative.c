577ca1a836e108486a329cf517fbbd69b1256508
Adamu Muhammad <sayyid211@gmail.com>
2022-06-03 09:50:31 -0700

    add header file to n+-0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,15 +20,15 @@ int main(void)
 	/* check the status of n*/
 	if (n == 0)
 	{
-		printf("%d, is zero\n", &n);
+		printf("%u, is zero\n", &n);
 	}
 	else if (n > 0)
 	{
-		printf("%d, is positive\n", &n);
+		printf("%u, is positive\n", &n);
 	}
 	else
 	{
-		printf("%d, is negative\n", &n);
+		printf("%u, is negative\n", &n);
 	}
 	return (0);
 }
