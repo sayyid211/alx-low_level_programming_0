1b41214bcd79f60dc6faa5ddb6e3c5841bdbed8f
Adamu Muhammad <sayyid211@gmail.com>
2022-06-02 20:27:09 -0700

    add print with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * puts does the printing job
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
