dc6bc47009e3255ae5d85e5bd1dd93bc91d0a826
Adamu Muhammad <sayyid211@gmail.com>
2022-06-03 12:40:24 -0700

    add alphaber U L case pt

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * program prints alphabets in lower and upper case
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 97;
+	int y = 65;
+	while (x < (x + 26)
+	{
+		putchar(x);
+		x++;
+	}
+	while (y < (y +25))
+	{
+		putchar(y);
+		y++;
+	}
+	putchar('\n');
+	return (0);
+}
+			
