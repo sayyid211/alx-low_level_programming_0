cc4d9207af5beabdc4d9a66ea48909947faa9da9
Adamu Muhammad <sayyid211@gmail.com>
2022-06-04 11:33:45 -0700

    add print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * printing 0 to 9 in decimal
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x <= 9)
+	{
+		printf("%d", x);
+		x++;
+	}
+	printf('\n');
+	return (0);
+}
