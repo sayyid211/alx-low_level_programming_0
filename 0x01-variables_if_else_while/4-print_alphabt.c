74a0476e56739e95ee040e6de497bd336ddd6855
Adamu Muhammad <sayyid211@gmail.com>
2022-06-04 11:18:17 -0700

    add print without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * program prints alphabets without q and e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		if (x != 'q'||'e')
+		{
+			putchar(x);
+		}
+		x++;
+	}
+	putchar('\n");
+return (0);
+}
