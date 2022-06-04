3a820743a584130830d2ebedb259faa267997075
Adamu Muhammad <sayyid211@gmail.com>
2022-06-04 12:26:03 -0700

    add print reversed alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * print reversed alphabets
+ * Return : Always 0
+ */
+int main(void)
+{
+	char x = 'z';
+
+	while (x >= 'a')
+	{
+		putchar(x);
+		x--;
+	}
+	putchar('\n');
+	return (0);
+}
