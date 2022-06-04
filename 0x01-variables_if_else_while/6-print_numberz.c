b3465e94171a404dc1e478cd460e1ad94384864c
Adamu Muhammad <sayyid211@gmail.com>
2022-06-04 11:53:55 -0700

    add print char nums

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,11 +3,12 @@
  * main - Entry point
  *
  * program to print 0 to 9 as chars
- * return: always 0
+ * Return: always 0
  */
 int main(void)
 {
 	int x = 48;
+
 	while (x <= 57)
 	{
 		putchar(x);
