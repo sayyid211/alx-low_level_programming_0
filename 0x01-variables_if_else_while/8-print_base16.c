aa125c16d3644894152ec78d93d6281f85574f46
Adamu Muhammad <sayyid211@gmail.com>
2022-06-04 12:41:35 -0700

    add print hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * print nums in base 16
+ * Return: always 0
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x < 16)
+	{
+		x = ('%x', x);
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
