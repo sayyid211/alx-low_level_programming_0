c40cad338a74601b432a21dd0fbe8d558a44f647
Adamu Muhammad <sayyid211@gmail.com>
2022-06-05 08:01:39 -0700

    update size checker

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,4 +17,5 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(s));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(t));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(q));
+	return (0);
 }
