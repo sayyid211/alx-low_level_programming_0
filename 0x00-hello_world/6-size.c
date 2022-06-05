8d28e331cdf34719edd7324d9d7c65f499569c81
Adamu Muhammad <sayyid211@gmail.com>
2022-06-05 08:00:02 -0700

    update size checker

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(p));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(r));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(s));
-	printf("Size of a long long int:  %lu byte(s)\n", (unsigned long)sizeof(t));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(t));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(q));
 }
