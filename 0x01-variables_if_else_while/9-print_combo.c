cf158f9c0af517a1e591d8e8789fe15e5ee63ad0
Adamu Muhammad <sayyid211@gmail.com>
2022-06-06 05:58:14 -0700

    add print combo

---
+++ 0x01-variables_if_else_while/9-print_combo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * program prints arabic numerals separated  by commas
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+       char x = 0;
+
+	while (x <= 9)
+	{
+		putchar(x);
+		putchar(',');
+		putchat(' ');
+	}
+	putchar('\n');
+	return (0);
+}
