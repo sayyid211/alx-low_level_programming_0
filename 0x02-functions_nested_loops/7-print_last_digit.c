79ad0c41161a1dfb910a0ee31a0e72f978b856bf
Adamu Muhammad <sayyid211@gmail.com>
2022-06-08 20:51:27 +0000

    print unit place value function

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a decimal number
+ * Return: returns the input's last digit
+ */
+int print_last_digit(int)
+{
+	int n;
+	int last_digit = n%10;
+	return (last_digit);
+}
