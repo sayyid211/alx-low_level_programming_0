87fe66b4ef743ec57ec785a38e5f795caf476d44
Adamu Muhammad <sayyid211@gmail.com>
2022-06-08 20:09:07 +0000

    update sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  * print_sign - Returns the sign of a given number, + - or 0
+ * n - function argument for print_sign
  * Return: Always 0 (Success)
  */
 int print_sign(int n)
 {
-	
 	if (n > 0)
 	{
 		_putchar('+');
-		return (0);
+		return (1);
 	}
 	else if (n == 0)
 	{
