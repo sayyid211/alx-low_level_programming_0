476c63d85b5b0aeaa3a9a32e67b2c32a19d42db4
Adamu Muhammad <sayyid211@gmail.com>
2022-06-08 20:59:36 +0000

    add print unit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
  * print_last_digit - prints the last digit of a decimal number
  * Return: returns the input's last digit
  */
-int print_last_digit(int n, int x)
+int print_last_digit(int n, int p)
 {
 	p = n%10;
 	return (p);
