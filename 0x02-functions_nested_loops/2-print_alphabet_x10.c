fc64869cb6c6daf499aeabdca58994b20f2433a5
Adamu Muhammad <sayyid211@gmail.com>
2022-06-08 15:55:04 +0000

    add print 10 x a-z

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print the alphabets in lower case 10x
+ */
+
+void print_alphabet_x10(void)
+{
+	int line_count = 0;
+	char alpha = 'a';
+
+	While (line_count < 10)
+	{
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
 int _putchar(int c);
 void print_alphabet(void);
 int printchar(int x);
-
+void print_alphabet_x10(void);
