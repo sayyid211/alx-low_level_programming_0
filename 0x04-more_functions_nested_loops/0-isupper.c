9f55ec3ba9281c875eceefc9729a047be45037ec
Adamu Muhammad <sayyid211@gmail.com>
2022-06-09 14:54:13 +0000

    add is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - checks the case of alphabets
+ *@c: argument to be tested
+ * Return: 1 if upper case and 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c < 97)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
