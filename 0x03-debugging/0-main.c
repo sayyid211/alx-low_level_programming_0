f50115ee726c8e88d3565c2c48e511dec4dbd135
Adamu Muhammad <sayyid211@gmail.com>
2022-06-09 11:47:12 +0000

    update main files

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,6 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
- * @i: variable to test
  * Return: 0
  */
 
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
+
 int positive_or_negative(int i);
-/* argument is changed to int since we are dealing with ints*/
-#endif /* MAIN_H*/
+
+#endif /* MAIN_H */
