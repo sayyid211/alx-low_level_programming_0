42fe344d1fdb3a39b77a21a546ee4ae890c3064d
Adamu Muhammad <sayyid211@gmail.com>
2022-06-09 21:54:56 -0700

    add isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isdigit - checks if input is a digit
+ *-Return: 1 for true, 0 for false
+ */
+_isupper(int c )
+{
+	if (c <= 48 <= 57)
+		return (1);
+	else
+		return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,5 +7,5 @@ int positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int _putchar(int c);
 int _isupper(int c);
-
+int _isdigit(int c);
 #endif /* MAIN_H */
