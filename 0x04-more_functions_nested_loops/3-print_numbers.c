746d2f546feba9422a9a41234834228e8ff72453
Adamu Muhammad <sayyid211@gmail.com>
2022-06-10 15:20:17 +0000

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - print 0-9
+ * @x: variavle to hold numbers
+ * @count: variable to count printing
+ */
+void print_numbers(void)
+{
+	int count = 0;
+	while (count <=1)
+	{
+		for (x = 0; x < 10; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
+}
