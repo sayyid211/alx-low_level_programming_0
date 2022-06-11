28fc43d2d3465addb96c6b82f638e6ea17dab4b9
Adamu Muhammad <sayyid211@gmail.com>
2022-06-11 21:32:52 +0000

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", x);
+		}
+	}
+	return (0);
+}
