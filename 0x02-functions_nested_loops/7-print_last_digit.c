50016e77f0153cd2534f29946d5334a3476de9d4
Adamu Muhammad <sayyid211@gmail.com>
2022-06-12 18:42:28 +0000

    last figure

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
  * print_last_digit - prints the last digit of a decimal number
- * @n: Argument to function 
+ * @n: Argument to function
  * Return: returns the input's last digit
  */
 int print_last_digit(int n)
 {
-	return (n%10);
+	int x = (n%10);
+
+	_putchar(x);
 	return (n%10);
 }
