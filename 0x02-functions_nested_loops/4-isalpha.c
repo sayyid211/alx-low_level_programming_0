9092cfd64ca091181a2e5783b471bb28d4f93fac
Adamu Muhammad <sayyid211@gmail.com>
2022-06-12 16:31:29 +0000

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isalpha - check if input is alphabetic
+ *
+ * @c: argument
+ * Return: 1 for true, 0 for false
+ */
+
+int _isalpha(int c)
+{
+	return(c > 64 < 91 && c > 96 < 123);
+}
