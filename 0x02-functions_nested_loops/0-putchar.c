b7819bed2e6ee6a10606e5e0110150c846530769
Adamu Muhammad <sayyid211@gmail.com>
2022-06-12 15:29:09 +0000

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,21 +4,16 @@
  *
  * Return: 0
  */
-int printchar(int x, char stg)
+int printchar(void)
 {
 
-	stg[]="_putchar";
-	x = 0;
+	char stg[] = "_putchar";
+	int x = 0;
 
 	while (x < 8)
 	{
 		_putchar(stg[x]);
 		x++;
 	}
-	return (0);
-}
-int main(void)
-{
-	printchar();
 	return (0);
 }
