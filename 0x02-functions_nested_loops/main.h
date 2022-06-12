91ffa9a600c824d9c0c62406bcfde8bffa64e38c
Adamu Muhammad <sayyid211@gmail.com>
2022-06-12 15:30:54 +0000

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 int _putchar(int c);
 void print_alphabet(void);
-int printchar(int x);
+int printchar(void);
 void print_alphabet_x10(void);
 int print_sign(int n);
 int print_last_digit(int n);
+int _islower(int c);
