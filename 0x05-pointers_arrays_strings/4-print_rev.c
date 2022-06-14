d7a12b9841ded908242992b62780fb3e04cc2eeb
Adamu Muhammad <sayyid211@gmail.com>
2022-06-14 16:15:29 +0000

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - print recerse string
+ * @s: string address
+ */
+
+void print_rev(char *s)
+{
+	int y;
+	int x = 0;
+
+	while (*(s + x) != 0)
+	{
+		x++;
+	}
+
+	y = x;
+	while (y >= 0)
+	{
+		_putchar(*(s + y));
+		y--;
+	}
+}
