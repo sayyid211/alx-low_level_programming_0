cfe5221579cd462be324a5a2b4579c5280de273e
Adamu Muhammad <sayyid211@gmail.com>
2022-06-14 18:29:55 +0000

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts2 - print string skipping odd index
+ * @str: argument
+ */
+void puts2(char *str)
+{
+	int x = 0;
+
+	while (*(str + x) != 0)
+	{
+		_putchar(*(str + x));
+		x = x + 2;
+	}
+}
