26e4387350f35c69cbb23fcd6344f76ffda3a0a9
Adamu Muhammad <sayyid211@gmail.com>
2022-06-14 17:17:31 +0000

    rev str

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rev_string - function for reversing string
+ * @s: argument to keep string
+ */
+
+void rev_string(char *s)
+{
+	int x = 0;
+	int y;
+	int x = 0;
+	char *tmp;
+
+	while(*(s + x) != 0)
+	{
+		x++;
+	}
+
+	y = x - 1;
+	while (y >= 0)
+	{
+		*(tmp + z) = *(s + y);
+		y--;
+		z++;
+	}
+	*s = *tmp;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,4 +6,5 @@ void swap_int(int *a, int *b);
 int _putchar(char c);
 void _puts(char *str);
 void print_rev(char *s);
+void rev_string(char *s);
 #endif /* MAIN_H */
