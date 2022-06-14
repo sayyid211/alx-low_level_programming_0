53a6ac45efbe947f053882cd4319f67bd7aff147
Adamu Muhammad <sayyid211@gmail.com>
2022-06-14 21:29:13 +0000

    print arr

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,3 +4,12 @@
  * @a: pointer to array address
  * @n: number of elements to print
  */
+void print_array(int *a, int n)
+{
+	int x = 0;
+	for (;x <= n; x++)
+	{
+		printf("%d, ",&a);
+		printf("\n");
+	}
+}
