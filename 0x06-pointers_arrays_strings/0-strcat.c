f106e88e7d19f718833b3e2b9914e88074176d1f
Adamu Muhammad <sayyid211@gmail.com>
2022-06-16 12:55:00 +0000

    conc str

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * _strcat - concatenates strings
+ * @dest: destination str
+ * @src: source of second str
+ * Return: conc str
+ */
+char *_strncat(char *dest, char *src)
+{
+	/* get dest size*/
+	int x = _strlen(dest);
+
+	/* variable to hold index of src */
+	int y = x - 1;
+	int i = 0;
+
+	/* loop through src, appending elements to dest */
+	while (y != 0)
+	{
+		*(dest + y) = *(src + i);
+		y++;
+		i++;
+	}
+	return (*dest);
+}
+
+/**
+ * _strlen - computes the length of a string
+ * @s: argument, pointer int to string array
+ * Return: size of string array
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != 0)
+	{
+		i++;
+	}
+	return (i);
+}
