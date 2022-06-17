167fb99bb00322dd750ae5f611567dfd16423bdd
Adamu Muhammad <sayyid211@gmail.com>
2022-06-17 10:19:35 +0000

    compr str

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,25 +1,27 @@
 #include "main.h"
 /**
- * _strcmp - compare 2 strings
+ * _strcmp - cxompare 2 strings
  * @s1: first string
  * @s2: second string
  * Return: 0 or 1
  */
 int _strcmp(char *s1, char *s2)
 {
-	int x;
+	int x = 0;
+	int y;
 
-	if (*s1 == *s2)
+	while (*(s1 + x) != '\0' || *(s2 + x) != '\0')
 	{
-		x = 0;
+		if (*(s1 + x) != *(s2 + x))
+		{
+			y = (*(s1 + x) - *(s2 + x));
+			break;
+		}
+		else
+		{
+			y = 0;
+		}
+		x++;
 	}
-	else if (*s1 < *s2)
-	{
-		x = -15;
-	}
-	else
-	{
-		x = 15;
-	}
-	return (x);
+	return (y);
 }
