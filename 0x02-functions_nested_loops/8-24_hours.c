1ae3fc63416e08fcb89708d651dfed392d197c8e
Adamu Muhammad <sayyid211@gmail.com>
2022-06-18 15:21:17 +0000

    echo time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * jack_bauer - print 24hrs
+ */
+void jack_bauer(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 24; x++)
+	{
+		for (y = 0; y < 60; y++)
+		{
+			if (x < 10)
+			{
+				_putchar(48);
+				_putchar(48 + x);
+				_putchar(58);
+				if (y < 10)
+				{
+					_putchar(48);
+					_putchar(48 + y);
+				}
+				else
+				{
+					_putchar(48 + y);
+				}
+			}
+			else
+			{
+				_putchar(48 + x);
+				_putchar(58);
+				if (y < 10)
+				{
+					_putchar(48);
+					_putchar(48 + y);
+				}
+				else
+				{
+					_putchar(48 + y);
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,4 +10,5 @@ int print_last_digit(int n);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int);
+void jack_bauer(void);
 #endif /* MAIN_H */
