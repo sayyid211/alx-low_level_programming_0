25c3de328439293e53ef9ee8ed72e1d814ddcf14
Adamu Muhammad <sayyid211@gmail.com>
2022-06-19 19:04:36 +0000

    copy str

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcpy - copy string
+ * @dest: copy destination
+ * @src: copy source
+ * Return: some string
+ */
+
+void *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
