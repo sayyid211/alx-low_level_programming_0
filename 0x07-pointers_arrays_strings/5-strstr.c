79c690f6da624d53a936ee161938fc6b71863cf0
Adamu Muhammad <sayyid211@gmail.com>
2022-06-21 12:06:59 +0000

    search substr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,10 +14,10 @@ char *_strstr(char *haystack, char *needle)
 		return (haystack);
 	while (*haystack)
 	{
-		if (*(haystack + i) == *(needle + i))
+		if (haystack[i] == needle[i])
 		{
 			do {
-				if (needle[i + 1] != '\0')
+				if (needle[i] != '\0')
 					return (haystack + i);
 				i++;
 			} while (haystack[i] == needle[i]);
