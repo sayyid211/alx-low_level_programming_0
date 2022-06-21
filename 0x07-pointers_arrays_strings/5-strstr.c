55f156d824656696aecf769798316524bdf3b359
Adamu Muhammad <sayyid211@gmail.com>
2022-06-21 11:55:39 +0000

    search substr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strstr - searches for a substr
+ * @haystack: where to search
+ * @needle: what to search for
+ * Return: Address of found str
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+
+	if (*needle == 0)
+	{
+		return (haystack);
+	}
+
+	while (*haystack)
+	{
+		if (*(haystack + i) == *(needle + i))
+		{
+			do {
+				if (needle[i + 1] != '\0')
+					return (haystack + i);
+				i++;
+			} while (haystack[i] == needle[i]);
+		}
+		haystack++;
+	}
+	return (NULL);
+}
