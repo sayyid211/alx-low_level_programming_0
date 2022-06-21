d3ebe35306d1a389b447601cfecb41283afac1d7
Adamu Muhammad <sayyid211@gmail.com>
2022-06-21 10:10:36 +0000

    search char

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,19 +9,13 @@
 char *_strchr(char *s, char c)
 {
 	int i = 0;
-	char *b;
 
-	while (*(s + i) != '\0')
+	while (s[i] != '\0')
 	{
-		if (*(s + i) == c)
+		if (s[i] == c)
 		{
-			b = (s + i);
-		}
-		else
-		{
-			b = NULL;
+			return (s + i);
 		}
 		i++;
 	}
-	return (b);
 }
