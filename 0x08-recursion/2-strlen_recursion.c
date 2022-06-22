cfc7bc1dda7699875b3bab88c57c1221174fa658
Adamu Muhammad <sayyid211@gmail.com>
2022-06-22 15:23:23 +0000

    count str

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns str length
+ * @s: str to count
+ * Return: str length
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*(s + i) == 0)
+	{
+		return (i);
+	}
+	return (_strlen_recursion(s + i + 1));
+}
