3c61a5307a5a511c6cad4b56954744761f55111a
Adamu Muhammad <sayyid211@gmail.com>
2022-06-22 16:39:20 +0000

    count str

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,10 +7,10 @@
 int _strlen_recursion(char *s)
 {
 	int i = 0;
-	
-	if (*(s + i) == '\0')
+
+	if (s[i] == '\0')
 	{
 		return (s + i);
 	}
-	return (_strlen_recursion(s +(s + i)));
+	return (_strlen_recursion(1 +(s + i)));
 }
