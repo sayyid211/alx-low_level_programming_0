d747c720b478ed3d58549a7a18314bddeda475c5
Adamu Muhammad <sayyid211@gmail.com>
2022-06-27 13:26:16 +0000

    count args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: success
+ */
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
