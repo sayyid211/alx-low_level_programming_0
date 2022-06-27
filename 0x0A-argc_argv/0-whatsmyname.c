41bc978cd7ea2294a9e73ac6b5d2cf88a5a18473
Adamu Muhammad <sayyid211@gmail.com>
2022-06-27 13:17:16 +0000

    my name is argv[0]

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: arguments count
+ * @argv: arguments vector
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
