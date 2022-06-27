54811cae9edee42df26244e128a3b40299a3d267
Adamu Muhammad <sayyid211@gmail.com>
2022-06-27 17:18:26 +0000

    print argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: arg count
+ * @argv: arg vect
+ * Return: Success
+ */
+
+int main(int argc, char *argv[])
+{
+	int x = 1;
+	for(;x < argc; x++)
+	{
+		printf("%s\n", argv[x]);
+	}
+	return (0);
+}
