5b5878370b0ead8825df29b4d6477448b8e68636
Adamu Muhammad <sayyid211@gmail.com>
2022-06-29 18:54:44 +0000

    init array using malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,13 +12,13 @@
 char *create_array(unsigned int size, char c)
 {
 	char *p;
+	unsigned int i;
 
 	while (1)
 	{
 		p = malloc(sizeof(char) * size);
 		if (p == NULL)
 			return (NULL);
-		unsigned int i;
 		for (i = 0; i < size; i++)
 		{
 			p[i] = c;
