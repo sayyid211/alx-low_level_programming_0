10754150681b3ba1fc84d31dd8a9691a7b55c336
Adamu Muhammad <sayyid211@gmail.com>
2022-06-29 20:04:06 +0000

    dup str

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+int _strlen(char *s);
+
+/**
+ * _strdup - duplicstes a string
+ * @str: string to duplicate
+ * Return: Success or failure
+ */
+
+char *_strdup(char *str)
+{
+	char *copy;
+	int size = _strlen(str);
+
+	if (str == NULL)
+		return (NULL);
+	while (1)
+	{
+		copy = malloc(sizeof(char) * size);
+		for (i = 0; i < size; i++)
+		{
+			copy[i] = str[i];
+		}
+		if (copy == NULL)
+			return (NULL);
+		return (copy);
+	}
+}
+
+/**
+ * _strlen - computes the length of a string
+ * @s: argument, pointer int to string array
+ * Return: size of string array
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != 0)
+	{
+		i++;
+	}
+	return (i);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -33,5 +33,6 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 
 #endif /* MAIN_H */
