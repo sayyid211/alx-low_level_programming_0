194cd8e25a3e121b05f6dcb92a8fa47e126394a5
Adamu Muhammad <sayyid211@gmail.com>
2022-06-29 16:47:23 +0000

    create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - makes an array of dynamic memory
+ * @size: size of array
+ * @c: arg to initialize first index with
+ * Return: pointer to array or null
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *p = malloc(sizeof(char) * size);
+	*p = c;
+	return (p);
+}
