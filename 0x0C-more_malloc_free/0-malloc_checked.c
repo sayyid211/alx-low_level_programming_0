bc122b72ad211a8f43c05c762c9cc93336d5e868
Adamu Muhammad <sayyid211@gmail.com>
2022-06-30 15:23:41 +0000

    mlc chk

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,3 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
+/**
+ * malloc_checked - allocates memory
+ * @b: argument
+ */
+
+void *_malloc_checked(unsigned int b)
+{
+	int *p;
+	while (1)
+	{
+		if (p == NULL)
+			return (98);
+		p = malloc(sizeof(p) * b);
+		return (p);
+	}
+}
