626dbc67246863e3a2eee88d0f84c00f610d0af6
Adamu Muhammad <sayyid211@gmail.com>
2022-06-30 13:24:43 +0000

    update p-types

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -38,6 +38,10 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 int _strlen(char *s);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
 
 
 #endif /* MAIN_H */
