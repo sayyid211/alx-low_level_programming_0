f3e2cf343f22af3af5becf960ead43f302d62a35
Adamu Muhammad <sayyid211@gmail.com>
2022-06-30 08:22:48 +0000

    dup str

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,7 +17,7 @@ char *_strdup(char *str)
 
 	while (1)
 	{
-		if (str == 'NULL')
+		if (_strlen(str) != _strlen(copy))
 			return (NULL);
 		copy = malloc(sizeof(char) * (size + 1));
 		if (copy == NULL)
