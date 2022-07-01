9707f5ca532b4261f717aec6dc9e1f0cb4a8787d
Adamu Muhammad <sayyid211@gmail.com>
2022-07-01 14:56:37 +0000

    my first static lib

---
+++ 0x09-static_libraries/0-putchar.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * printchar - print given string
+ */
+void printchar(void)
+{
+
+	char stg[] = "_putchar";
+	int x = 0;
+
+	while (x < 8)
+	{
+		_putchar(stg[x]);
+		x++;
+	}
+}
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	printchar();
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit - checks if input is a digit
+ * @c: argument to check
+ * Return: 1 for true, 0 for false
+ */
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
