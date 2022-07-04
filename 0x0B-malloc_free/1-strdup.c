3735644e1b14403e3cf9d1484e1ce8ea7d8f92c6
Adamu Muhammad <sayyid211@gmail.com>
2022-07-04 13:47:44 +0000

    __FILE__

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,7 +15,7 @@ char *_strdup(char *str)
 	int size = _strlen(str);
 	int i;
 
-	if (str == NULL)
+	if (str[i] == NULL)
 		return (NULL);
 	while (1)
 	{
--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Success
+ */
+
 int main(void)
 {
 	printf("%s\n",__FILE__);
