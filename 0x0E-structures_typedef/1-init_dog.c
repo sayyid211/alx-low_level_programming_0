52960668f1edcce5019f3456d1f3e3c3ef96b9fa
Adamu Muhammad <sayyid211@gmail.com>
2022-07-05 02:58:09 +0000

    init dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+/**
+ * init_dog - initializes a dog struct
+ * @d: arg 1
+ * @name: name of dog
+ * @age: dog age
+ * @owner: Dog owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog *d = {Karl, 2.20, Brian};
+}
---
+++ 0x0E-structures_typedef/main.h~
@@ -0,0 +1,4 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
