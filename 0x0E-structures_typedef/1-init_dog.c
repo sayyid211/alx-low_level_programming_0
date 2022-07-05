df6c03ff621f218dbf93e5e0f180a5003655cbe6
Adamu Muhammad <sayyid211@gmail.com>
2022-07-05 15:23:42 +0000

    dog init

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include "main.h"
 /**
  * init_dog - initializes a dog struct
  * @d: arg 1
@@ -8,5 +9,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *d = {Karl, 2.20, Brian};
+	struct dog *d;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
