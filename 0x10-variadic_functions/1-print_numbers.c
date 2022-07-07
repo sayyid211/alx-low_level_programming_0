7f23c3dcd89137270cb37c47a2b2385f3c1954e8
Adamu Muhammad <sayyid211@gmail.com>
2022-07-07 20:31:08 +0000

    correct print varaidic args

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,12 +11,12 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list list;
-	unsigned int x = 0;
+	unsigned int x = 1;
 
 	va_start(list, n);
 	while (x <= n)
 	{
-		printf("%u",va_arg(list,unsigned int));
+		printf("%u", va_arg(list, unsigned int));
 		if (separator != NULL)
 			printf("%p", separator);
 		x++;
