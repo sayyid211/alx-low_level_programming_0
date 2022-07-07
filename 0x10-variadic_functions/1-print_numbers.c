4dfcc68981e9a3a30e76954ad76c489d90471aa2
Adamu Muhammad <sayyid211@gmail.com>
2022-07-07 20:27:00 +0000

    print varaidic args

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - print numbers passed as args
+ * @separator: seperates numbers
+ * @n: first number
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int x = 1;
+
+	va_start(list, n);
+	while (x <= n)
+	{
+		printf("%u",va_arg(list,unsigned int));
+		if (separator != NULL)
+			printf("%p", separator);
+		x++;
+	}
+	_putchar('\n');
+	va_end (list);
+}
