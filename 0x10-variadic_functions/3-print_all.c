5403b93e92e36eb0f3e9ad414b824a560c8dd6bb
Adamu Muhammad <sayyid211@gmail.com>
2022-07-08 22:31:17 +0000

    variadic print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,6 +12,10 @@ void print_all(const char * const format, ...)
 	va_list val;
 	int val_num = strlen(format);
 	int counter = 0;
+	char p;
+	double q;
+	int r;
+	char *s;
 
 	va_start(val, format);
 	while (counter < val_num)
@@ -19,26 +23,31 @@ void print_all(const char * const format, ...)
 		switch(format[counter])
 		{
 		case 'c':
-			char x = va_arg(val, char);
-			printf("%c", x);
+			p = va_arg(val, int);
+			printf("%c", p);
+			break;
 		case 'f':
-			double x = va_arg(val, double);
-			printF("%f", x);
+			q = va_arg(val, double);
+			printf("%f", q);
+			break;
 		case 'i':
-			int x = Va_arg(val, int);
-			printf("%d", x);
+			r = va_arg(val, int);
+			printf("%d", r);
+			break;
 		case 's':
-			char *x = va_arg(val, char *);
-			if (x == NULL)
+			s = va_arg(val, char *);
+			if (s == NULL)
 			{
 				printf("(nil)");
 			}
 			else
 			{
-				printf("%s", x);
+				printf("%s", s);
 			}
+			break;
 		default:
 			break;
 		}
+		counter++;
 	}
 }
